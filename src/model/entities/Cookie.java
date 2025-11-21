5d6d8966252d5abcc7b83a929a4b1761d4742b6d
Clovis Luan <luanmdearaujo@gmail.com>
2025-11-21 11:44:06 -0300

    Criaçao da classe Cookie

---
+++ src/model/entities/Cookie.java
@@ -0,0 +1,30 @@
+package model.entities;
+
+public class Cookie {
+
+	private String nomeCookie;
+	private double precoCookie;
+
+	public Cookie(String nomeCookie, double precoCookie) {
+		this.nomeCookie = nomeCookie;
+		this.precoCookie = precoCookie;
+	}
+
+	
+	public String getNomeCookie() {
+		return nomeCookie;
+	}
+
+	public void setNomeCookie(String nomeCookie) {
+		this.nomeCookie = nomeCookie;
+	}
+
+	public double getPrecoCookie() {
+		return precoCookie;
+	}
+
+	public void setPrecoCookie(double precoCookie) {
+		this.precoCookie = precoCookie;
+	}
+
+}
