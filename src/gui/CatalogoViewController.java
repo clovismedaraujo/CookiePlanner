462500387fea713562a76a1cfe87be1c7e87672b
Clovis Luan <luanmdearaujo@gmail.com>
2025-11-21 02:59:59 -0300

    Criaçao do controller do catalogoView

---
+++ src/gui/CatalogoViewController.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CatalogoViewController {
+
+	@FXML
+	private TextField nomeCookieField;
+	
+	@FXML
+	private TextField valorCookieField;
+	
+	@FXML
+	private Button registraCatalogoButton;
+	
+}
